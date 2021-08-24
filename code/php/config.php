1ef8e254fbde83e6e835243fb6fdb210904155d2
Ulti <maxime.lopes@hotmail.com>
2021-08-24 20:45:55 +0200

    fixe url

--- code/php/config.php
+++ code/php/config.php
@@ -13,7 +13,7 @@ const DOSSIER_ASSETS = __DIR__ . '/assets';
 const DOSSIER_MEDIA = __DIR__ . 'media/';
 const DOSSIER_TYPO = __DIR__ . 'typo/';
 
-const BASE_URL = 'http://localhost/site/projet_portfolio/';
+const BASE_URL = 'http://localhost/projet_portfolio/';
 const MAX_FILE_SIZE = 5_000_000; // Taille max des fichiers (5 Mo)
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD);
