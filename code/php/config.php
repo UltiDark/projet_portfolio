12fedc5baf4565476e61cd9f93f9eacac6fc72cc
Ulti <maxime.lopes@hotmail.com>
2021-10-12 09:18:42 +0200

    maj

--- code/php/config.php
+++ code/php/config.php
@@ -5,7 +5,6 @@ const DB_USER = 'root';
 const DB_PASSWORD = '';
 const DB_NAME = 'portfolio';
 
-
 const DOSSIER_MODELS = __DIR__ . '/src/models';
 const DOSSIER_VIEWS = __DIR__ . '/views';
 const DOSSIER_CONTROLLERS = __DIR__ . '/src/controllers';
@@ -13,7 +12,7 @@ const DOSSIER_ASSETS = __DIR__ . '/assets';
 const DOSSIER_MEDIA = __DIR__ . 'media/';
 const DOSSIER_TYPO = __DIR__ . 'typo/';
 
-const BASE_URL = 'http://localhost/projet_portfolio/';
+const BASE_URL = 'http://localhost/Projet/portfolio/';
 const MAX_FILE_SIZE = 5_000_000; // Taille max des fichiers (5 Mo)
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD);
